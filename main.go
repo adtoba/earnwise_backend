45e8ee6cc32b0ec6b9e05d13e8b6bf2edce9a596
adtoba <adetoba53@gmail.com>
2026-02-12 16:36:04 +0100

    feat: Added database connection

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/adtoba/earnwise_backend.git/src/initializers"
+	"github.com/gin-gonic/gin"
 )
 
+var (
+	server *gin.Engine
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	initializers.ConnectDB(&config)
+
+	server = gin.Default()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	config, err := initializers.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	server.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	log.Fatal(server.Run(":" + config.Port))
 }
